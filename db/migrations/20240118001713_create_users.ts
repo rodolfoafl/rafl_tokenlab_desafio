c3481aec17725984a1b73fee05d155ff542cfdd9
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2024-01-17 21:21:06 -0300

    Created users migration

---
+++ db/migrations/20240118001713_create_users.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.string('password').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
