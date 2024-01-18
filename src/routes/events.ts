fbbc0a60d9eed8e81ff1a86de0390525cd613f85
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2024-01-17 23:29:30 -0300

    Added events routes & integration with db

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,55 +1,132 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+import crypto from 'node:crypto'
+import { knex } from '../database'
 
 export async function eventsRoutes(app: FastifyInstance) {
-  app.post('/', async (req, reply) => {
+  app.get(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (req, reply) => {
+      const getEventParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getEventParamsSchema.parse(req.params)
+      if (!id) {
+        return reply.status(400).send()
+      }
+
+      const { userSessionId } = req.cookies
+
+      const event = await knex('events')
+        .where({ id, userId: userSessionId })
+        .first()
+
+      return {
+        event,
+      }
+    },
+  )
+
+  app.post('/', { preHandler: [checkSessionIdExists] }, async (req, reply) => {
     const createEventBodySchema = z.object({
       description: z.string(),
       startDate: z.string(),
       endDate: z.string(),
     })
 
-    // userId - get from session cookie
+    const { userSessionId } = req.cookies
 
     const { description, startDate, endDate } = createEventBodySchema.parse(
       req.body,
     )
-    console.log(description, startDate, endDate)
-    // TODO: create event
 
-    return reply.status(201).send()
-  })
+    const existingEvent = await knex('events')
+      .where({
+        userId: userSessionId,
+        startDate,
+        endDate,
+      })
+      .first()
 
-  app.put('/:id', async (req, reply) => {
-    const getEventParamsSchema = z.object({
-      // id: z.string().uuid(),
-      id: z.string(),
+    if (existingEvent) {
+      return reply.status(409).send()
+    }
+
+    await knex('events').insert({
+      id: crypto.randomUUID(),
+      description,
+      startDate,
+      endDate,
+      userId: userSessionId,
     })
 
-    const { id } = getEventParamsSchema.parse(req.params)
-    console.log(id, req.body)
-    // TODO: update event
-
     return reply.status(201).send()
   })
 
-  app.delete('/:id', async (req, reply) => {
-    const getEventParamsSchema = z.object({
-      // id: z.string().uuid(),
-      id: z.string(),
-    })
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (req, reply) => {
+      const getEventParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
 
-    const { id } = getEventParamsSchema.parse(req.params)
-    console.log(id, req.body)
-    // TODO: remove event
+      const { id } = getEventParamsSchema.parse(req.params)
 
-    return reply.status(201).send()
-  })
+      if (!id || !req.body || !Object.keys(req.body).length) {
+        return reply.status(400).send()
+      }
 
-  app.get('/', async () => {
-    const events: never[] = []
-    console.log(events)
-    // TODO: list events
+      const { userSessionId } = req.cookies
+
+      const event = await knex('events')
+        .where({ id, userId: userSessionId })
+        .first()
+
+      if (!event) {
+        return reply.status(404).send()
+      }
+
+      await knex('events').update(req.body).where({ id, userId: userSessionId })
+
+      return reply.status(200).send()
+    },
+  )
+
+  app.delete(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (req, reply) => {
+      const getEventParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { userSessionId } = req.cookies
+
+      const { id } = getEventParamsSchema.parse(req.params)
+
+      const event = await knex('events')
+        .where({ id, userId: userSessionId })
+        .first()
+
+      if (!event) {
+        return reply.status(404).send()
+      }
+
+      await knex('events').where({ id, userId: userSessionId }).delete()
+
+      return reply.status(200).send()
+    },
+  )
+
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { userSessionId } = req.cookies
+
+    const events = await knex('events').where({ userId: userSessionId })
 
     return {
       events,
